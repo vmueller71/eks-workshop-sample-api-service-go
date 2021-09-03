1949db436916ac30c8490631a8543c20d119ac16
Volker Mueller <vmueller71@gmail.com>
2021-09-03 10:14:59 -0500

    fixed syntax error

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello EKS workshop"!}
+		res := &response{Message: "Hello EKS workshop!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
